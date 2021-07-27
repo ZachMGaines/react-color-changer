4012332d1a149a283b5fb96260b9a3ca18a41131
Zachary Gaines <zacharygaines28@gmail.com>
2021-07-27 10:25:34 -0700

    color randomizer complete

---
+++ src/components/app/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ColorRandomizer from '../color-randomizer/color-randomizer';
+
+export default function App() {
+  return <ColorRandomizer />
+}
---
+++ src/components/color-randomizer/color-randomizer.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+export default class ColorRandomizer extends Component {
+  state = {
+    selectedColor: 'green'
+  }
+
+  handleColorChange = color => {
+    this.setState({ selectedColor: color });
+  }
+
+  componentDidMount() {
+    const colors = ['green', 'pink', 'orange'];
+    const random = colors[Math.floor(Math.random() * colors.length)];
+    this.setState({ selectedColor: random });
+    setInterval(() => {
+      const random = colors[Math.floor(Math.random() * colors.length)];
+      this.setState({ selectedColor: random });
+    }, 1000
+    );
+
+
+  }
+
+  render() {
+
+
+    return (
+      <>
+        < div
+          style={{
+            backgroundColor: this.state.selectedColor,
+            width: '100px',
+            height: '100px'
+          }
+          }
+        ></div>
+      </>
+    );
+  }
+}
